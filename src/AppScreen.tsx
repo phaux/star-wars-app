0c2a59c698671e83ed98b8d8f77ddb7cb375fd47
Nikita Stefaniak <phaux4@gmail.com>
2019-10-03 14:03:45 +0200

    fix: fix some warnings

--- src/AppScreen.tsx
+++ src/AppScreen.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, Tab, Tabs, Box } from "@material-ui/core"
+import { AppBar, Container, Tab, Tabs, Box, Typography } from "@material-ui/core"
 import * as React from "react"
 import { useHistory, useParams } from "react-router-dom"
 import SwipeableViews from "react-swipeable-views"
@@ -14,15 +14,24 @@ export const AppScreen: React.FC = () => {
   return (
     <>
       <AppBar position="sticky" color="default">
-        <Tabs value={params.screen} onChange={(ev, screen) => history.push(`/${screen}`)} centered>
+        <Tabs
+          value={params.screen != null ? params.screen : false}
+          onChange={(ev, screen) => history.push(`/${screen}`)}
+          centered
+        >
           <Tab value="people" label="👤 People" />
           <Tab value="starships" label="🚀 Starships" />
         </Tabs>
       </AppBar>
       <SwipeableViews
-        index={SCREENS.indexOf(params.screen as any)}
+        index={SCREENS.indexOf(params.screen as any) + 1}
         onChangeIndex={(ev, idx) => history.push(`/${SCREENS[idx]}`)}
       >
+        <Box clone py={6}>
+          <Typography variant="h4" color="textSecondary" align="center">
+            Select game mode
+          </Typography>
+        </Box>
         <Box overflow="hidden">
           <Container maxWidth="md">
             <PeopleScreen />
